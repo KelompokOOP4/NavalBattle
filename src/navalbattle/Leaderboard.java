527ac6ea7c25e4119ed85c831dd0940145d6efe9
Rizkyhamm <rizkyhamm@github.com>
2022-12-16 05:47:09 +0700

    leaderboard minor fix

--- src/navalbattle/Leaderboard.java
+++ src/navalbattle/Leaderboard.java
@@ -8,16 +8,22 @@ public class Leaderboard {
  private String playerID;
  private int playerScore;
     public Leaderboard(String name, int score){
-    public String getPlayerID() {
-        return playerID;
-    } public void setPlayerID(String playerID) {
-        this.playerID = playerID;
-    } public int getPlayerScore() {
-        return playerScore;
-    } public void setPlayerScore(int playerScore) {
-        this.playerScore = playerScore;
-    }
+        public String getPlayerID() {
+            return playerID;
+        } public void setPlayerID(String playerID) {
+            this.playerID = playerID;
+        } public int getPlayerScore() {
+            return playerScore;
+        } public void setPlayerScore(int playerScore) {
+            this.playerScore = playerScore;
+        }
     playerID = name;
     playerScore = score;
-    } 
+    }
+    if (board.isEmpty()){
+label1.setText (winnerName);
+scoreLabel1.setText (winnerScore);
+} else {
+}
+}
 }
