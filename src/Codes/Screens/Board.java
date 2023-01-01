f62724a9e22e0c3aa91e00548cc0e239cfaa264a
slayalir <slayalir@upi.edu>
2023-01-01 15:40:30 +0700

    resize bg lagi

--- src/Codes/Screens/Board.java
+++ src/Codes/Screens/Board.java
@@ -3,11 +3,55 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Codes.Screens;
-
-/**
+/*
  *
- * @author slaya
+ * @author slayalir
  */
-public class Board {
-    
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+ 
+public class Board extends JPanel implements ActionListener {
+    Dude p;
+    public Image img;
+    Timer time;
+ 
+    public Board() {
+        p = new Dude();
+        addKeyListener(new AL());
+        setFocusable(true);
+        ImageIcon i = new ImageIcon (this.getClass().getResource("/gambar/backgroundocean1.png"));
+        img = i.getImage();
+        time = new Timer(5, this);
+        time.start();
+    }
+ 
+    public void actionPerformed(ActionEvent e) {
+        p.move();
+        repaint();
+    }
+ 
+    public void paint(Graphics g) {
+        super.paint(g);
+        Graphics2D g2d = (Graphics2D) g;
+        if ((p.getX() - 60) % 1600 == 0)// p.getX() == 590 || p.getX() == 2990) 590 & 2400
+            p.nx = 0;
+        if ((p.getX() - 860) % 1600 == 0)// p.getX() == 1790 || p.getX() == 4190) 1790 & 2400
+            p.nx2 = 0;
+        g2d.drawImage(img, 785-p.nx2, 0, null);
+        if (p.getX() > 60)  //590
+            g2d.drawImage(img, 785 - p.nx, 0, null); //685 - p.getnX
+        
+        g2d.drawImage(p.getImage(), 50, p.getY(), null);
+    }
+ 
+    private class AL extends KeyAdapter {
+        public void keyReleased(KeyEvent e) {
+            p.keyReleased(e);
+        }
+ 
+        public void keyPressed(KeyEvent e) {
+            p.keyPressed(e);
+        }
+    }
 }
--- src/Codes/Screens/Frame.java
+++ src/Codes/Screens/Frame.java
@@ -3,11 +3,24 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Codes.Screens;
-
-/**
+/*
  *
- * @author slaya
+ * @author slayalir
  */
+/*import javax.swing.*;
+
 public class Frame {
-    
-}
+    public Frame () {
+        JFrame frame = new JFrame ();
+        frame.add (new Board ());
+        frame.setTitle("Test Game");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 450); //700 & 365
+        frame.setResizable(true);
+        frame.setVisible (true);
+        frame.setLocationRelativeTo(null);
+    }
+    public static void main (String [] args) {
+        new Frame ();
+    }
+}*/
