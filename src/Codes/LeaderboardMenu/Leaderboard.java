f0a8027b4cfeb284168c9796970371a633609a2b
Rizkyhamm <rizkyhamm@github.com>
2023-01-01 16:58:10 +0700

    bismillah solve

---
+++ src/Codes/LeaderboardMenu/Leaderboard.java
@@ -0,0 +1,74 @@
+package Codes.LeaderboardMenu;
+import java.sql.*;
+import java.util.ArrayList;
+
+
+/**
+ *
+ * @author rizky
+ */
+
+public class Leaderboard {
+    private Connection con;
+    private String url;
+    private String uname;
+    private String pass;
+    private Statement stmt;
+    private String query;
+    
+    public Leaderboard(){
+        url = "jdbc:mysql://localhost/navalbattle";
+        uname = "root";
+        pass = "";
+        setConnectionAndStatement();
+    }
+    
+    public void setConnectionAndStatement(){
+        try {
+            con = DriverManager.getConnection(url, uname, pass);
+            stmt = con.createStatement();
+        } catch (SQLException ex) {
+            System.err.print(ex.getMessage());
+            System.exit(1);
+        }
+    }
+    
+    public Leaderboard leaderboard(){
+        Leaderboard leaderboard = null;
+        
+        try{
+            query = "SELECT * FROM leaderboard ORDER BY score DESC LIMIT 1";
+            ResultSet rs = stmt.executeQuery(query);
+            
+            while(rs.next()){
+                leaderboard = new Leaderboard(
+                rs.getString("name"),
+                rs.getInt("score"));
+            }
+            
+            if(leaderboard == null){
+                System.out.println("Data tidak ditemukan");
+                System.exit(0);
+            }
+        } catch(SQLException ex){
+            System.err.print("Error getting the data: "+ ex.getMessage());
+            System.exit(1);
+        }
+        return leaderboard;
+    }
+    
+    
+    public void save (Leaderboard leaderboard){
+        try {
+            query = "INSERT INTO historysingleplayer (name, score) VALUES ('%s', '%d')";
+            query = String.format(query, 
+                    score.getName(),
+                    score.getScore(),
+            stmt.executeUpdate(query);
+            System.out.println("Congratulation for your score!");
+        }catch (SQLException ex) {
+            System.err.print("Error inserting score " + ex.getMessage());
+            System.exit(1);
+        }
+    }
+}
--- src/Codes/LeaderboardMenu/Score.java
+++ src/Codes/LeaderboardMenu/Score.java
@@ -1,8 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package Codes;
+package Codes.LeaderboardMenu;
 
 /**
  *
@@ -11,17 +7,25 @@ package Codes;
 public class Score {
     private String name;
     private int score;
+
+    public Score(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public int getScore() {
         return score;
     }
+
     public void setScore(int score) {
         this.score = score;
     }
-    
 }
