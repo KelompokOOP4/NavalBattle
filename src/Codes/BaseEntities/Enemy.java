df0f1c25b374447eb54946c95ad117b855dfbc2e
IhsanHilmi <hilmi.kyosou.080202@gmail.com>
2023-01-02 01:17:26 +0700

    add handler dan Enemy

---
+++ src/Codes/BaseEntities/Enemy.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Codes.BaseEntities;
+
+/**
+ *
+ * @author Abui
+ */
+public class Enemy extends Ship{
+
+    @Override
+    public void setDefaultValues() {
+    }
+
+    @Override
+    public void getImage() {
+    }
+
+    @Override
+    public void update() {
+    }
+    
+}
---
+++ src/Codes/Handlers/ObstacleHandler.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Codes.Handlers;
+
+import Codes.BaseEntities.Obstacle;
+import java.awt.Graphics2D;
+
+/**
+ *
+ * @author Abui
+ */
+public class ObstacleHandler extends OtherObjectHandler{
+
+    private Obstacle ob;
+    
+    @Override
+    public void update() {
+    }
+
+    @Override
+    public void draw(Graphics2D g2d) {
+    }
+}
