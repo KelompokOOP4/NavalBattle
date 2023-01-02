fd4b881f4317b3b4df201a124461be3937a1504c
slayalir <slayalir@upi.edu>
2023-01-02 10:07:03 +0700

    update bg

--- src/Codes/Screens/GameScreen.java
+++ src/Codes/Screens/GameScreen.java
@@ -53,7 +53,7 @@ public class GameScreen extends JPanel implements Runnable{
         this.addKeyListener(p2c);
         this.setFocusable(true);
         this.isMultiplayer = isMultiplayer;
-        ImageIcon i = new ImageIcon (this.getClass().getResource("/Resources/bg_fix.png"));
+        ImageIcon i = new ImageIcon (this.getClass().getResource("/Resources/backgroundocean1.png"));
         img1 = i.getImage();
         img2 = i.getImage();
         img3 = i.getImage();
